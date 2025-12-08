a68ffe26d5d8c175255a0be899c6b1cce6dcc4cc
moonsj50 <moonsj50@gmail.com>
2025-12-08 16:37:37 +0900

    [Game] GameState, GameInstance 생성

---
+++ Source/AFO/Public/Game/AFGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "AFGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AFO_API UAFGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
