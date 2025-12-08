04c8cf139a1b08153c1807a816a3479212e1927d
moonsj50 <moonsj50@gmail.com>
2025-12-08 22:19:17 +0900

    [GameState] 팀스코어, 시간, 게임단계 구현완료
    TeamRedScore, TeamBlueScore, RemainingTimeSeconds, CurrentGamePhase 변수 복제
    OnRep_RemainingTime, SetRemainingTime, SetGamePhase 함수 구현

--- Source/AFO/Private/Game/AFGameState.cpp
+++ Source/AFO/Private/Game/AFGameState.cpp
@@ -1,5 +1,47 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// AFGameState.cpp
 
 
 #include "Game/AFGameState.h"
+#include "Net/UnrealNetwork.h"
 
+void AAFGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// Score 변수 복제
+	DOREPLIFETIME(ThisClass, TeamRedScore);
+	DOREPLIFETIME(ThisClass, TeamBlueScore);
+
+	// 남은 시간 변수 복제 및 RepNotify 설정
+	DOREPLIFETIME(ThisClass, RemainingTimeSeconds);
+
+	// Phase 변수 복제
+	DOREPLIFETIME(ThisClass, CurrentGamePhase);
+}
+
+// RepNotify 함수 구현
+void AAFGameState::OnRep_RemainingTime()
+{
+	// 인게임 UI에서 이 함수를 호출하기
+}
+
+
+// 서버 권한 : 시간 설정
+void AAFGameState::SetRemainingTime(int32 NewTime)
+{
+	if (GetLocalRole() == ROLE_Authority)
+	{
+		RemainingTimeSeconds = NewTime;
+		OnRep_RemainingTime();
+	}
+}
+
+// 서버 권한 : 게임 단계 설정
+void AAFGameState::SetGamePhase(EAFGamePhase NewPhase)
+{
+	if (GetLocalRole() == ROLE_Authority)
+	{
+		CurrentGamePhase = NewPhase;
+		// 재현님 이 변수 변경을 토대로 화면 전환 해주시면 됩니다
+	}
+}
--- Source/AFO/Public/Game/AFGameState.h
+++ Source/AFO/Public/Game/AFGameState.h
@@ -1,4 +1,4 @@
-// 
+// AFGameState.h
 
 #pragma once
 
