ba152714033ddc80b7a9109e6621f6fde0cf845d
soloplayer-tech <nos36650@gmail.com>
2025-12-09 19:38:51 +0900

    Title Menu Update
    아직 미완성

---
+++ Source/AFO/Private/UI/UW_TitleLayout.cpp
@@ -0,0 +1,50 @@
+#include "UI/UW_TitleLayout.h"
+#include "Components/Button.h"
+#include "Components/EditableText.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Player/AFTitlePlayerController.h"
+
+UUW_TitleLayout::UUW_TitleLayout(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UUW_TitleLayout::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (PlayButton)
+	{
+		PlayButton->OnClicked.AddDynamic(this, &ThisClass::OnPlayButtonClicked);
+	}
+
+	if (ExitButton)
+	{
+		ExitButton->OnClicked.AddDynamic(this, &ThisClass::OnExitButtonClicked);
+	}
+
+	if (OptionButton)
+	{
+		OptionButton->OnClicked.AddDynamic(this, &ThisClass::OnOptionButtonClicked);
+	}
+}
+
+void UUW_TitleLayout::OnPlayButtonClicked()
+{
+	AAFTitlePlayerController* PlayerController = GetOwningPlayer<AAFTitlePlayerController>();
+	if (IsValid(PlayerController) == true)
+	{
+		FText ServerIP = ServerIPEditableText->GetText();
+		PlayerController->JoinServer(ServerIP.ToString());
+	}
+}
+
+void UUW_TitleLayout::OnExitButtonClicked()
+{
+	UKismetSystemLibrary::QuitGame(this, GetOwningPlayer(), EQuitPreference::Quit, false);
+}
+
+void UUW_TitleLayout::OnOptionButtonClicked()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Option Button Clicked!"));
+}
