48a6b855f2e048631c608c1c37f04a3aace52f86
soloplayer-tech <nos36650@gmail.com>
2025-12-09 17:30:42 +0900

    .

---
+++ Source/AFO/Controller/AFTitlePlayerController.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AFTitlePlayerController.generated.h"
+
+
+UCLASS()
+class AFO_API AAFTitlePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	virtual void BeginPlay() override;
+
+	void JoinServer(const FString& InIPAddress);
+
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = ASUIPlayerController, Meta = (AllowPrivateAccess))
+	TSubclassOf<UUserWidget> UIWidgetClass;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = ASUIPlayerController, Meta = (AllowPrivateAccess))
+	TObjectPtr<UUserWidget> UIWidgetInstance;
+
+};
