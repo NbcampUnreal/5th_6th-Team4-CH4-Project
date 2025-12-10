46c613eea86ebc7cb4fdea45acb1b9a4410a6c6d
soloplayer-tech <nos36650@gmail.com>
2025-12-10 20:34:48 +0900

    간단한 게임모드 작성
    아직 Title에서 BattleZone으로 넘어갈 때 게임모드가 자동으로 안바뀌는거 고치지 못함

--- Source/AFO/Public/Game/AFGameMode.h
+++ Source/AFO/Public/Game/AFGameMode.h
@@ -1,20 +1,35 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameMode.h"
 #include "AFGameMode.generated.h"
 
-class AFPlayerController;
+class AAFGameState;
+class APlayerController;
 
 UCLASS()
 class AFO_API AAFGameMode : public AGameMode
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	virtual void PostLogin(APlayerController* NewPlayer) override;
+    AAFGameMode();
 
-	virtual void Logout(AController* Exiting) override;
+    virtual void BeginPlay() override;
+    virtual void PostLogin(APlayerController* NewPlayer) override;
+    virtual void Logout(AController* Exiting) override;
+
+    void ReportKill(APlayerController* Killer, APlayerController* Victim);
+
+private:
+    int32 RoundDuration;
+    FTimerHandle RoundTimerHandle;
+
+    TMap<APlayerController*, uint8> PlayerTeams;
+
+    void StartRound();
+    void TickRound();
+    void EndRound();
+
+    AAFGameState* GetAFGameState() const;
 };
