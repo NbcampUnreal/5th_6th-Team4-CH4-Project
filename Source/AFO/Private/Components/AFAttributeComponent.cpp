ee7393e4f789756b05fb5652d643679190384df8
guapo02 <guapo1212@naver.com>
2025-12-10 20:50:01 +0900

    Components 폴더 생성 후 AttributeComponent 추가

---
+++ Source/AFO/Private/Components/AFAttributeComponent.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/AFAttributeComponent.h"
+#include "GameFramework/Actor.h"
+
+UAFAttributeComponent::UAFAttributeComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false; 
+}
+
+void UAFAttributeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	Health = MaxHealth;
+}
+
+void UAFAttributeComponent::ApplyDamage(float Damage)
+{
+	if (Damage <= 0.f) return;
+
+	Health -= Damage;
+	Health = FMath::Clamp(Health, 0.f, MaxHealth);
+
+	UE_LOG(LogTemp, Warning, TEXT("%s HP: %f"), *GetOwner()->GetName(), Health);
+
+	if (Health <= 0.f)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s is Dead!"), *GetOwner()->GetName());
+	}
+}
+
